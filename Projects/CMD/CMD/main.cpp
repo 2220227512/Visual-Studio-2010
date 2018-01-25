c2369fdc600e4d22a7293566a139b0377b24340c
2220227512 <l18224005064>
2018-01-25 14:47:48 +0800

    c++上传
    第一次上传

---
+++ Projects/CMD/CMD/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+	printf("ni hao a");
+	getchar();
+
+	//system("ipconfig");
+	system("calc");
+	system("pause");
+
+}
---
+++ Projects/Hello2017/Hello2017/Hello.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <Windows.h>
+  //标dfdfd;
+
+void main(){
+	//printf("111111111");
+   // #include "lsjhel.txt"
+	//getchar();   //标记;
+//	MessageBox(0,"n内容","标题",0);
+
+	ShellExecute(0,"open","notepad"
+		,0,0,1);
+
+
+}
